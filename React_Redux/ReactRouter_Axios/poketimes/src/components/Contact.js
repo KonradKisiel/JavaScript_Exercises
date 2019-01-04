a570788d6631ecb638f264bf8d767762dbddddf4
KonradKisiel <konrad.kisiel.webmaster@gmail.com>
2019-01-04 18:39:15 +0100

    React Router + Axios Blog

---
+++ React_Redux/ReactRouter_Axios/poketimes/src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+        error, tempora quidem in impedit eveniet iusto vel nulla rerum, officia
+        nesciunt illo doloremque placeat architecto magnam. Porro ducimus eos
+        atque!
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
